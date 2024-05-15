b093cefb6a22d3a9c949d559678fe8c5e6207f55
Russell Ian Bulatao <108667456+ri-bulatao@users.noreply.github.com>
2024-05-16 01:25:47 +0800

    updates

--- app/Http/Controllers/Resident/PaymayaController.php
+++ app/Http/Controllers/Resident/PaymayaController.php
@@ -60,7 +60,7 @@ class PaymayaController extends Controller
                     ->setFirstName($this->user->first_name)
                     ->setMiddleName($this->user->middle_name)
                     ->setLastName($this->user->last_name)
-                    ->setBirthday(Carbon::parse($this->user->birth_date)->format('Y-m-d'))
+                    ->setBirthday(Carbon::parse($this->user->birth_date))
                     ->setGender($this->user->gender === 'male' ? 'M' : 'F')
                     ->setContact(
                         (new Contact())
